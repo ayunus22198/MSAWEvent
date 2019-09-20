1b7aed49a965027753ebf316ae4d935a7ffb9b48
Adnan <ayunus@ucsc.edu>
2019-09-19 18:29:01 -0700

    Backend Files

---
+++ backend-msaw/server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+/**
+mongoose.Promise = global.Promise; // let's us use then catch
+mongoose.set('useCreateIndex', true);
+mongoose.connect(``, { useNewUrlParser: true });
+mongoose.connection
+    .once('open', () => console.log('Mongodb running'))
+    .on('error', err => console.log(err)); // to use routes
+**/
+const app = express();
+
+//lets us access/write JSON objects and push to database
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit:50000}));
+app.use(morgan('dev')); //debugging for HTTP requests
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`server is listening on PORT: ${PORT}`));
