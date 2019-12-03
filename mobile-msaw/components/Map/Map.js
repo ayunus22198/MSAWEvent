83a9f925b7164c75ff301c92422fec347e23e318
alinafarhat77 <57518232+alinafarhat77@users.noreply.github.com>
2019-12-03 14:05:54 -0800

    Update Map.js

--- mobile-msaw/components/Map/Map.js
+++ mobile-msaw/components/Map/Map.js
@@ -20,12 +20,63 @@ export default class Map extends Component {
         address: '146, Hutchison Dr, Davis, CA 95616',
         coords: { latitude: 38.538120, longitude: -121.771610 },
         ratings: []
-      }, {
+      }, 
+        {
         name: 'Sams Restaurant',
         address: '301 B St, Davis, CA 95616',
         coords: { latitude: 38.544150, longitude: -121.745240 },
         ratings: []
-      }]
+      },
+                   
+        {
+        name: 'Ikes Sandwiches',
+        address: '212 F St #4515, Davis, CA 95616',
+        coords: { latitude: 38.544044, longitude:  -121.739462 },
+        ratings: []
+      },
+                    
+        {
+        name: 'House Of Shah Afghan Urban Eats',
+        address: '538 Main St, Woodland, CA 95695',
+        coords: { latitude: 38.677573, longitude:  -121.772905 },
+        ratings: []
+      },
+                    
+        {
+        name: 'Preethi Indian Cuisine',
+        address: '715 2nd St, Davis, CA 95616',
+        coords: { latitude: 38.543952, longitude:  -121.738938 },
+        ratings: []
+      },
+                    
+        {
+        name: 'The Halal Guys',
+        address: '500 1st St #7, Davis, CA 95616',
+        coords: { latitude: 38.541314, longitude:  -121.739967 },
+        ratings: []
+      },
+                    
+        {
+        name: 'TxMx Grill',
+        address: '250 W Quad, Davis, CA 95616',
+        coords: { latitude: 38.542330, longitude:  -121.749118 },
+        ratings: []
+      },
+                    
+        {
+        name: 'Yeti Restaurant',
+        address: '234 E St, Davis, CA 95616',
+        coords: { latitude: 38.544473, longitude:  -121.740574 },
+        ratings: []
+      },
+                    
+        {
+        name: 'Chickpeas Kitchen',
+        address: '640 W Covell Blvd, Davis, CA 95616',
+        coords: { latitude: 38.560018, longitude:  -121.756175 },
+        ratings: []
+      }
+                ]
     }
   }
 
