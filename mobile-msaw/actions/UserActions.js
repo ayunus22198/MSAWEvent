1007e6245c858704e48e3d6a5b4a6ccd381795f3
Adnan <ayunus@ucsc.edu>
2020-01-09 00:09:29 -0800

    ss

--- mobile-msaw/actions/UserActions.js
+++ mobile-msaw/actions/UserActions.js
@@ -2,7 +2,7 @@ import { USER_LOGIN, UPDATE_SCHEDULE } from './types';
 import axios from 'axios';
 
 export const userLogin = (user) => dispatch => {
-  axios.post('https://ff58c38a.ngrok.io/api/login', { user }).then((response)=> {
+  axios.post('https://7ebd18d2.ngrok.io/api/login', { user }).then((response)=> {
     dispatch({
       type: USER_LOGIN,
       payload: { user }
@@ -11,7 +11,7 @@ export const userLogin = (user) => dispatch => {
 }
 
 export const userRetrieve = (user) => dispatch => {
-  axios.get('https://ff58c38a.ngrok.io/api/login', { user }).then((response)=> {
+  axios.get('https://7ebd18d2.ngrok.io/api/login', { user }).then((response)=> {
     dispatch({
       type: USER_LOGIN,
       payload: { user }
@@ -20,7 +20,7 @@ export const userRetrieve = (user) => dispatch => {
 }
 
 export const userSubmitSelectableEvent = (idOfEvent, userPhoneId) => dispatch => {
-  axios.post('https://ff58c38a.ngrok.io/api/selectableEvent', { idOfEvent, userPhoneId }).then((user)=> {
+  axios.post('https://7ebd18d2.ngrok.io/api/selectableEvent', { idOfEvent, userPhoneId }).then((user)=> {
     dispatch({
       type: UPDATE_SCHEDULE,
       payload: { userPhoneId, idOfEvent }
